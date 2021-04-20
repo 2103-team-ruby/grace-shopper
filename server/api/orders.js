12b939f93db474bfef586b83d0a993b129856dab
Keon <keonjukes@gmail.com>
2021-04-20 14:38:39 -0400

    api-routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -22,32 +22,5 @@ router.get('/:userId', async (req, res, next) => {
     }
 })
 
-//PUT /api/orders/:orderId
-// router.put('/:orderId', async(req, res, next) => {
-// try {
-//     const orderId = req.params.orderId;
-//     const cart = await ProductOrder.findOne({
-//         where: {
-//             orderId: orderId
-//         }
-//     })
-//     res.status(204).send(await cart.update(req.body))
-// } catch(error) {
-//     next(error)
-// }
 
-// })
-
-
-//DELETE /api/orders/:productId
-// router.delete('/:productId', async(req, res, next) => {
-//     try {
-//         const productId = req.params.productId;
-//         const product = await Product.findByPk(productId)
-//         await Order.removeProduct(product)
-//         res.sendStatus(204);
-//       }catch(error) {
-//         next(error)
-//         }
-// })
 
