6693820ce012345e54b3bc1793c15ce8bbeffb9a
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-04-21 16:52:23 -0400

    BE: feat: Adds route to edit quantity in cart and route to complete order

--- server/api/users.js
+++ server/api/users.js
@@ -3,15 +3,11 @@ const User = require("../db/models/user");
 const ProductOrder = require("../db/models/productOrder");
 const Order = require("../db/models/order");
 const Product = require("../db/models/product");
-const e = require("express");
 module.exports = router;
 
 router.get("/", async (req, res, next) => {
 	try {
 		const users = await User.findAll({
-			// explicitly select only the id and username fields - even though
-			// users' passwords are encrypted, it won't help if we just
-			// send everything to anyone who asks!
 			attributes: ["id", "username"],
 		});
 		res.json(users);
@@ -20,7 +16,6 @@ router.get("/", async (req, res, next) => {
 	}
 });
 
-// GET /api/users/:id/cart
 router.get("/:id/cart", async (req, res, next) => {
 	try {
 		const order = await Order.findOne({
@@ -35,7 +30,6 @@ router.get("/:id/cart", async (req, res, next) => {
 	} catch (error) {}
 });
 
-// POST /api/users/:id/cart
 router.post("/:id/cart", async (req, res, next) => {
 	try {
 		const newproductId = req.body.productId;
@@ -82,7 +76,34 @@ router.post("/:id/cart", async (req, res, next) => {
 	}
 });
 
-// DELETE /api/users/:id/cart/:itemId
+router.put("/:id/cart/:itemId", async (req, res, next) => {
+	try {
+		const { quantity } = req.body;
+		const product = await Product.findByPk(req.params.itemId);
+
+		let order = await Order.findOne({
+			where: {
+				userId: req.params.id,
+				status: "incomplete",
+			},
+			include: [{ model: ProductOrder }],
+		});
+
+		const cartItems = order.productOrders;
+
+		const currentCartItems = cartItems.map((cartItem) => cartItem.productId);
+		const itemIdx = currentCartItems.indexOf(Number(req.params.itemId));
+
+		const itemInCart = cartItems[itemIdx];
+		itemInCart.quantity = quantity;
+		itemInCart.subtotal = product.price * itemInCart.quantity;
+		await itemInCart.save();
+		res.send(cartItems);
+	} catch (error) {
+		next(error);
+	}
+});
+
 router.delete("/:id/cart/:itemId", async (req, res, next) => {
 	try {
 		const order = await Order.findOne({
@@ -93,6 +114,15 @@ router.delete("/:id/cart/:itemId", async (req, res, next) => {
 		});
 		order.removeProduct(req.params.itemId);
 		res.status(204).send();
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.put("/:id/order/:orderId", async (req, res, next) => {
+	try {
+		const order = await Order.findByPk(req.params.orderId);
+		res.send(await order.update(req.body));
 	} catch (error) {
 		next(error);
 	}
