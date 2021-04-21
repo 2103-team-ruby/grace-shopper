73fa73bd59eab5803bb87d9aad6490d7a88a357f
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-04-21 11:54:38 -0400

    BE: Adds route to fetch user cart

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,35 @@
-const router = require('express').Router()
-const { models: { User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const User = require("../db/models/user");
+const ProductOrder = require("../db/models/productOrder");
+const Order = require("../db/models/order");
+const Product = require("../db/models/product");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
-  try {
-    const users = await User.findAll({
-      // explicitly select only the id and username fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'username']
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
-  }
-})
+router.get("/", async (req, res, next) => {
+	try {
+		const users = await User.findAll({
+			// explicitly select only the id and username fields - even though
+			// users' passwords are encrypted, it won't help if we just
+			// send everything to anyone who asks!
+			attributes: ["id", "username"],
+		});
+		res.json(users);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// GET /api/users/:id/cart
+router.get("/:id/cart", async (req, res, next) => {
+	try {
+		const order = await Order.findOne({
+			where: {
+				userId: req.params.id,
+				status: "incomplete",
+			},
+			include: [{ model: ProductOrder }],
+		});
+
+		order ? res.send(order.productOrders) : res.send([]);
+	} catch (error) {}
+});
