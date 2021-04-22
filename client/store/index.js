41adb5a2cec232646dac12b02075e591a68d9fd2
Altus Szawlowski <altusszawlowski@gmail.com>
2021-04-22 11:05:26 -0400

    (FE) feature for redux store

--- client/store/index.js
+++ client/store/index.js
@@ -1,20 +1,22 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import allProductsReducer from './allProducts'
-import singleProductReducer from './singleProduct'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
+import allProductsReducer from "./allProducts";
+import singleProductReducer from "./singleProduct";
+import cartReducer from "./cart";
 
 const reducer = combineReducers({
   auth,
-  singleProduct : singleProductReducer,
-  allProducts : allProductsReducer
-})
+  singleProduct: singleProductReducer,
+  allProducts: allProductsReducer,
+  cartProducts: cartReducer,
+});
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from "./auth";
