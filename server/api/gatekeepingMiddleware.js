b7d4e03eedc5272777645e30f8f459d881a7393f
Brendan Bettencourt <bbettenc@nd.edu>
2021-04-27 00:42:10 -0400

    BE: Feat Gatekeeping Middleware for Security

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -11,30 +11,14 @@ const requireToken = async (req, res, next) => {
   }
 };
 
-function isAdmin(req, res, next) {
+function isAdmin( req, res, next) {
   if (req.user.isAdmin) {
     return next();
   }
   res.send("Sorry you do not have admin privileges");
 }
 
-function isCorrectUser(req, res, next) {
-  if (req.params.id === req.user.id) {
-    return next();
-  }
-  res.send("Incorrect Username or Password");
-}
-
-function isCorrectUserOrAdmin(req, res, next) {
-  if (isAdmin || isCorrectUser) {
-    return next();
-  }
-  res.send("You do not have proper permissions");
-}
-
 module.exports = {
   requireToken,
   isAdmin,
-  isCorrectUser,
-  isCorrectUserOrAdmin,
 };
