08b81ae917e9f7580fb0da673060ba8ae51492aa
Brendan Bettencourt <bbettenc@nd.edu>
2021-04-27 12:55:43 -0400

    FE: Refactor Displays proper prices/ decimals

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -32,7 +32,7 @@ export class AllProducts extends React.Component {
 												<Link to={`/products/${product.id}`}>
 													<h3>{product.name}</h3>
 												</Link>
-												<p>${product.price}</p>
+												<p>${product.price / Math.pow(10,2)}</p>
 											</div>
 										</div>
 									</div>
